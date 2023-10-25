4b3e571ac4156cee6ed93afd23bf4c860c3d58fb
NEHA2837 <nehamishra59098@gmail.com>
2023-10-25 21:13:29 +0530

    new file

---
+++ set.js
@@ -0,0 +1,6 @@
+//document.write("hello")
+
+document.getElementById("a").innerHTML = "<center><h2> PNINFOSYS Gwalior 123 </h2>"
+document.getElementById("a").setAttribute("style","border:10px dotted red");
+//document.getElementById("a").removeAttribute("style");
+//document.getElementById("b").innerHTML = "<h1>GWALIOR</h1>";
